1689f8ac2a3507fbac8ede9edb2ed81b187b989d
Ankita Kumari <68110313+ankita9809@users.noreply.github.com>
2024-05-30 22:21:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Input } from "./scomp/ui/input";
 import Login from "./components/Login";
